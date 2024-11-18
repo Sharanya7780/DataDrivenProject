c7e5797dc2dbd21d22747896e781f7ad35f85446
k10sh <k10sh@sharanya>
2024-11-18 17:20:05 +0530

    First Commit

---
+++ DataDriven_Framework/src/test/java/applicationLayer/AdminLogin.java
@@ -0,0 +1,23 @@
+package applicationLayer;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AdminLogin {
+	//define repository
+		@FindBy(name="txtUsername")
+		WebElement Objuser;
+		@FindBy(xpath="//input[@id='txtPassword']")
+		WebElement Objpass;
+		@FindBy(id="btnLogin")
+		WebElement ObjLogin;
+		
+		//write method for login
+		public void verifyLogin(String username,String password)
+		{
+			Objuser.sendKeys(username);
+			Objpass.sendKeys(password);
+			ObjLogin.click();
+		}
+		
+}
---
+++ DataDriven_Framework/src/test/java/applicationLayer/AdminLogout.java
@@ -0,0 +1,19 @@
+package applicationLayer;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AdminLogout {
+	@FindBy(xpath="//a[@id='welcome']")
+	WebElement clickWelcome;
+	@FindBy(xpath="//a[normalize-space()='Logout']")
+	WebElement clickLgout;
+	public void verifyLogout()throws Throwable
+	{
+		clickWelcome.click();
+		clickLgout.click();
+	}
+}
+
+
+
